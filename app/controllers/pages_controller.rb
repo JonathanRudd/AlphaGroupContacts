f4d505c81a9dc82bb10da8813bc3c3b4f4e8c4f2
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-11-04 00:57:21 +0900

    homepage logic for @users

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
-    @users = User.all
+
+#    @department = Department.find(current_user.department_id)
+
+    @users = User.where(department_id: current_user.department_id)
+
   end
 end
