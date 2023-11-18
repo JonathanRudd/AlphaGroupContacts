0ad9d39a1e9402bbab0c13e1fe6343f4e69c1e4c
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-11-18 17:57:36 +0900

    fixed precompile manifest bug

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
 //= link popper.js
 //= link bootstrap.min.js
+//= link application.js
