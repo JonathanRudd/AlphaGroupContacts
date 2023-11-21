b67757bd5dbd3cd252b555219d6c9245236e9972
Jonathan Rudd <jonathanrudd3@gmail.com>
2023-11-21 15:30:11 +0900

    User search functionality in PagesController and update home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,9 +4,25 @@ class PagesController < ApplicationController
   def home
     if user_signed_in?
       @users = User.where(department_id: current_user.department_id)
+      @users =
+          if params[:query].present?
+            User.joins(:profile).where('profiles.first_name LIKE ? OR profiles.last_name LIKE ?', "%#{params[:query]}%", "%#{params[:query]}%")
+          else
+            User.all
+          end
     else
       @users = []
     end
   end
+
+  def index
+    @users =
+    if params[:query].present?
+      User.joins(:profile).where('profiles.first_name LIKE ? OR profiles.last_name LIKE ?', "%#{params[:query]}%", "%#{params[:query]}%")
+    else
+      User.all
+    end
+  end
+
 
 end
